113dafeb8b874199656b36e79ed3a87062d50ef5
alaa03omari <omari2003aloosh@gmail.com>
2026-06-29 12:31:12 +0300

    some changes

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'crudproject.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_db",
+        "USER": "django_user",
+        "PASSWORD": "django_pass",
+        "HOST": "db",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
